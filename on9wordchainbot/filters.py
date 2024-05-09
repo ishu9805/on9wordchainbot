f91f0a982e66248d81ecd05f89e31fc99316c7ec
ishu9805 <abhishek47903@gmail.com>
2024-05-09 18:58:00 +0530

    Update filters.py

--- on9wordchainbot/filters.py
+++ on9wordchainbot/filters.py
@@ -1,6 +1,6 @@
 from aiogram import types
 from aiogram import Dispatcher
-from aiogram.dispatcher.filters import BoundFilter
+from aiogram.Dispatcher.filters import BoundFilter
 
 from .constants import OWNER_ID, VIP
 
